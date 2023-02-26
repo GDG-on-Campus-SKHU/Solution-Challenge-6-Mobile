13d75fe527f4804da09dd5546a8b29096b78fcb3
MojitoBar <wnehdtjr5@gmail.com>
2023-02-26 23:48:55 +0900

    quize card click event added

--- bep/lib/MainView/quizeCard.dart
+++ bep/lib/MainView/quizeCard.dart
@@ -1,11 +1,32 @@
 import 'package:bep/Util/quizeType.dart';
 import 'package:flutter/material.dart';
 
-class quizeCard extends StatelessWidget {
+class quizeCard extends StatefulWidget {
+  final int index;
+  final List<bool> selectedList;
   final quizeType type;
   final String question;
+  final ValueChanged<int> onSelected;
 
-  quizeCard({required this.type, required this.question});
+  quizeCard({
+    required this.index,
+    required this.selectedList,
+    required this.type,
+    required this.question,
+    required this.onSelected,
+  });
+
+  @override
+  State<quizeCard> createState() => _quizeCardState();
+}
+
+class _quizeCardState extends State<quizeCard> {
+  Color selected = Color.fromRGBO(0, 0, 0, 0.25);
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,52 +38,71 @@ class quizeCard extends StatelessWidget {
         height: 60,
         width: screenSize.width - 32,
         decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(10)),
+          borderRadius: BorderRadius.circular(10),
           boxShadow: [
             BoxShadow(
-              color: Color.fromRGBO(0, 0, 0, 0.25),
-              blurRadius: 4,
+              color: !widget.selectedList[widget.index]
+                  ? Color.fromRGBO(0, 0, 0, 0.25)
+                  : Color.fromRGBO(255, 141, 25, 0.25),
               offset: Offset.zero,
-            ),
+              blurRadius: 5,
+            )
           ],
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
-              child: Container(
-                width: 35,
-                height: 35,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color.fromRGBO(0, 0, 0, 0.25),
-                      blurRadius: 4,
-                      offset: Offset.zero,
-                    )
-                  ],
-                  borderRadius: BorderRadius.all(Radius.circular(18)),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: Image(
-                    image: AssetImage('assets/tree.png'),
+        child: ElevatedButton(
+          onPressed: () {
+            setState(() {
+              widget.onSelected(widget.index);
+            });
+          },
+          style: ElevatedButton.styleFrom(
+            shadowColor: Color.fromARGB(0, 0, 0, 0),
+            padding: EdgeInsets.fromLTRB(12, 0, 0, 0),
+            backgroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 12, 0),
+                child: Container(
+                  width: 35,
+                  height: 35,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: !widget.selectedList[widget.index]
+                            ? Color.fromRGBO(0, 0, 0, 0.25)
+                            : Color.fromRGBO(255, 141, 25, 0.25),
+                        blurRadius: 4,
+                        offset: Offset.zero,
+                      )
+                    ],
+                    borderRadius: BorderRadius.all(Radius.circular(18)),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Image(
+                      image: AssetImage(
+                          'assets/${quizeTypeToString(widget.type)}.png'),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Container(
-              width: screenSize.width - 107,
-              child: Text(
-                question,
-                style: TextStyle(fontSize: 14),
-                softWrap: true,
+              Container(
+                width: screenSize.width - 100,
+                child: Text(
+                  widget.question,
+                  style: TextStyle(fontSize: 14),
+                  softWrap: true,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- bep/lib/MainView/quizeCardContainer.dart
+++ bep/lib/MainView/quizeCardContainer.dart
@@ -2,13 +2,38 @@ import 'package:bep/MainView/quizeCard.dart';
 import 'package:bep/Util/quizeType.dart';
 import 'package:flutter/material.dart';
 
-class quizeCardContainer extends StatelessWidget {
+class quizeCardContainer extends StatefulWidget {
   quizeCardContainer({required this.isQuizeOpen});
   final bool isQuizeOpen;
+
+  @override
+  State<quizeCardContainer> createState() => _quizeCardContainerState();
+}
+
+class _quizeCardContainerState extends State<quizeCardContainer> {
+  late List<bool> _isSelectedList;
+
+  @override
+  void initState() {
+    super.initState();
+    _isSelectedList = List.generate(3, (_) => false);
+  }
+
+  void _onSelect(int index) {
+    setState(() {
+      if (_isSelectedList[index]) {
+        _isSelectedList = List.generate(3, (_) => false);
+      } else {
+        _isSelectedList = List.generate(3, (_) => false);
+        _isSelectedList[index] = true;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Visibility(
-      visible: isQuizeOpen,
+      visible: widget.isQuizeOpen,
       child: Container(
         alignment: Alignment.bottomCenter,
         child: Padding(
@@ -17,14 +42,26 @@ class quizeCardContainer extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
               quizeCard(
-                  type: quizeType.Land,
-                  question: "Find the countries that plant the most trees"),
+                index: 0,
+                selectedList: _isSelectedList,
+                type: quizeType.Land,
+                question: "Find the countries that plant the most trees",
+                onSelected: _onSelect,
+              ),
               quizeCard(
-                  type: quizeType.Land,
-                  question: "Find a country where a variety of grasses grow"),
+                index: 1,
+                selectedList: _isSelectedList,
+                type: quizeType.Land,
+                question: "Find a country where a variety of grasses grow",
+                onSelected: _onSelect,
+              ),
               quizeCard(
-                  type: quizeType.Land,
-                  question: "Find the countries that plant the most trees"),
+                index: 2,
+                selectedList: _isSelectedList,
+                type: quizeType.Land,
+                question: "Find the countries that plant the most trees",
+                onSelected: _onSelect,
+              ),
             ],
           ),
         ),
