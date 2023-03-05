bb7833770ce5dda5f386681ca744f8ee190950d2
hanni66 <haeunkim0807@gmail.com>
2023-03-05 23:37:27 +0900

    modal view add hyperlink

--- bep/lib/ModalView/modalView.dart
+++ bep/lib/ModalView/modalView.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 Widget modalView(_width, context) {
   return Container(
@@ -48,13 +49,25 @@ Widget modalView(_width, context) {
               alignment: Alignment.topLeft,
               child: Padding(
                 padding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 74.0),
-                child: Text(
-                  '― Google',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    fontSize: 16,
-                    height: 1.5,
-                    color: Color.fromRGBO(73, 124, 255, 1.0),
+                child: InkWell(
+                  onTap: () async {
+                    String query = 'france';
+                    Uri uri =
+                        Uri.parse('https://www.google.com/search?q=$query');
+                    if (await canLaunchUrl(uri)) {
+                      await launchUrl(uri);
+                    } else {
+                      throw 'Could not launch $uri';
+                    }
+                  },
+                  child: Text(
+                    '― Google',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 16,
+                      height: 1.5,
+                      color: Color.fromRGBO(73, 124, 255, 1.0),
+                    ),
                   ),
                 ),
               ),
