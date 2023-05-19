d9730f821327a06992969dd0b1b45671c9e93f64
MojitoBar <wnehdtjr5@gmail.com>
2023-05-19 23:59:01 +0900

    주소 변경

--- bep/lib/Api/dioSetting.dart
+++ bep/lib/Api/dioSetting.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 Dio createDioClient() {
   Dio dio = Dio(
     BaseOptions(
-      baseUrl: 'https://bepserver.duckdns.org',
+      baseUrl: 'https://bep-server-skr6jumqta-uc.a.run.app',
     ),
   );
   return dio;
