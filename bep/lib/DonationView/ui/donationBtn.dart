627f154df315b1a2d72076e7bb1dc4ca1b846802
hanni66 <haeunkim0807@gmail.com>
2023-05-23 21:41:47 +0900

    donated button 추가

---
+++ bep/lib/DonationView/ui/donationBtn.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class DonationBtn extends StatefulWidget {
+  @override
+  State<DonationBtn> createState() => _DonationBtnState();
+}
+
+class _DonationBtnState extends State<DonationBtn> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: GestureDetector(
+          onTap: () {
+            print("\$1 Donate");
+          },
+          child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                children: [
+                  Text('\$1 Donated'),
+                  Image.asset(
+                    'assets/images/paypal-donate-button.png',
+                    width: 200,
+                    height: 80,
+                    fit: BoxFit.fill,
+                  ),
+                ],
+              ))),
+    );
+  }
+}
